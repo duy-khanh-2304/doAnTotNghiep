db0e24eb3a68bb3f4cd251d47008b2130b620a36
Duy Khanh <ngokhanh357@gmail.com>
2023-05-18 23:34:39 +0700

    add news and events

--- config/plugins.js
+++ config/plugins.js
@@ -15,5 +15,16 @@ module.exports = ({ env }) => ({
       },
     },
   },
+  slugify: {
+    enabled: true,
+    config: {
+      contentTypes: {
+        news: {
+          field: 'slug',
+          references: 'title',
+        },
+      },
+    },
+  },
   // ...
 });
